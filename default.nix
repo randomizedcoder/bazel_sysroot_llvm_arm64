827340763343beb4cfc8a4fa566e4bc536636eb3
randomizedcoder dave.seddon.ca@gmail.com <dave.seddon.ca@gmail.com>
2025-06-02 17:00:10 -0700

    GNU tool symlinks

--- default.nix
+++ default.nix
@@ -6,6 +6,24 @@ let
   clang = pkgs.llvmPackages_20.libcxxClang;
   lld = pkgs.llvmPackages_20.lld;
   libcxx = pkgs.llvmPackages_20.libcxx;
+  # List of GNU tool symlinks to LLVM equivalents
+  symlinks = [
+    { link = "ld"; target = "ld.lld"; }
+    { link = "objcopy"; target = "llvm-objcopy"; }
+    { link = "strip"; target = "llvm-strip"; }
+    { link = "ar"; target = "llvm-ar"; }
+    { link = "nm"; target = "llvm-nm"; }
+    { link = "ranlib"; target = "llvm-ranlib"; }
+    { link = "size"; target = "llvm-size"; }
+    { link = "strings"; target = "llvm-strings"; }
+    { link = "addr2line"; target = "llvm-addr2line"; }
+    { link = "c++filt"; target = "llvm-cxxfilt"; }
+    { link = "readelf"; target = "llvm-readelf"; }
+    { link = "elfedit"; target = "llvm-elfedit"; }
+    { link = "as"; target = "llvm-as"; }
+    { link = "cc"; target = "clang"; }
+    { link = "cc++"; target = "clang++"; }
+  ];
 in
 pkgs.stdenv.mkDerivation {
   name = "bazel-llvm-arm64";
@@ -20,7 +38,6 @@ pkgs.stdenv.mkDerivation {
     # Copy LLVM binaries
     echo "Copying LLVM binaries..."
     if [ -d "${llvmToolchain}/bin" ]; then
-      # Exclude llvm-exegesis as it's a large benchmarking tool not needed for compilation
       find "${llvmToolchain}/bin" -type f ! -name "llvm-exegesis" -exec cp -L {} $out/bin/ \;
     fi
     if [ -d "${clang}/bin" ]; then
@@ -36,14 +53,23 @@ pkgs.stdenv.mkDerivation {
       cp -r ${pkgs.coreutils}/bin/* $out/bin/ || true
     fi
 
-    # Create cc and cc++ symlinks in the same directory
-    (cd $out/bin && ln -sf clang cc && ln -sf clang++ cc++)
+    # Create symlinks for GNU tool names to LLVM equivalents
+    # This is required because Bazel and some build systems expect standard GNU tool names (e.g., 'ld', 'objcopy', 'strip'),
+    # but the sysroot only provides LLVM equivalents (e.g., 'ld.lld', 'llvm-objcopy', 'llvm-strip').
+    # By creating these symlinks, we ensure compatibility with Bazel's toolchain expectations.
+    cd $out/bin
+    for symlink in ${builtins.toString (map (s: ''${s.link}:${s.target}'') symlinks)}; do
+      link="$(echo $symlink | cut -d: -f1)"
+      target="$(echo $symlink | cut -d: -f2)"
+      if [ -e "$target" ]; then
+        ln -sf $target $link
+      fi
+    done
+    cd -
 
     # Copy LLVM libraries
     echo "Copying LLVM libraries..."
     if [ -d "${llvmToolchain}/lib" ]; then
-      # Exclude llvm-exegesis as it's a large benchmarking tool (75MB) not needed for compilation
-      # See https://llvm.org/docs/CommandGuide/llvm-exegesis.html for details
       find "${llvmToolchain}/lib" -type f -name "*.so*" ! -name "*exegesis*" -exec cp -L {} $out/lib/ \;
     fi
     if [ -d "${clang}/lib" ]; then
